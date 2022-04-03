0882c3f38352ee81437399a0849c68629aea57f4
jf <j@b.f>
2022-04-03 16:04:29 +0100

    generate docs for everything in peote.view

--- generateAPIdoc.sh
+++ generateAPIdoc.sh
@@ -2,10 +2,10 @@ haxe -xml doc/api/doc.xml \
 	-cp src -lib lime -D doc-gen \
 	--no-output -neko dummy.n \
 	peote.view.PeoteView
-
+	
 haxelib run dox \
-	-in "^peote\.view\.(PeoteView|Display|Program|Texture|Color)$" \
 	-i doc/api/doc.xml \
-	-o doc/api
+	-o doc/api \
+	--toplevel-package peote.view
 
 rm doc/api/doc.xml
